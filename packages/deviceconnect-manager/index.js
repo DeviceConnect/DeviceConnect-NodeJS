e4155099458765c7f8a180dea53ffa5d11efe162
Masaru Takano <mtaka@gclue.jp>
2016-03-03 19:04:55 +0900

    Launch HTTPS server.

--- packages/deviceconnect-manager/index.js
+++ packages/deviceconnect-manager/index.js
@@ -1,8 +1,19 @@
 var http = require('http');
+var https = require('https');
+var fs = require('fs');
 var manager = require('./deviceconnect-manager');
 var config = require('./config');
 var httpPort = config.httpServer.port;
+var httpsPort = config.httpsServer.port;
+var certificate = config.httpsServer.certificate;
+var privateKey = config.httpsServer.privateKey;
 
 http.createServer(manager).listen(httpPort);
+https.createServer({
+  cert: fs.readFileSync(certificate),
+  key: fs.readFileSync(privateKey)
+}, manager).listen(httpsPort);
 
-console.log('Device Connect Manager is running on:\n    - http://localhost:' + httpPort);
+console.log('Device Connect Manager is running on:\n'
+  + '    - http://localhost:' + httpPort + '\n'
+  + '    - https://localhost:' + httpsPort);
