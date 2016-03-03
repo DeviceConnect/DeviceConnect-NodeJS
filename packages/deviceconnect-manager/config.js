24a03aeba9212b765a7979f5325e32b8eb962250
Masaru Takano <mtaka@gclue.jp>
2016-03-03 19:53:22 +0900

    Enable to turn HTTPS server off with config.json.

--- packages/deviceconnect-manager/config.js
+++ packages/deviceconnect-manager/config.js
@@ -1,4 +1,23 @@
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
+config.http = createServerConfig(config.http);
+if (config.http.port === undefined) {
+    config.http.port = 4035;
+}
+config.https = createServerConfig(config.https);
+if (config.https.port === undefined) {
+    config.https.port = 4036;
+}
+
 module.exports = config;
+
+function createServerConfig(server) {
+    if (server === undefined) {
+        server = {};
+    }
+    if (server.enabled === undefined) {
+        server.enabled = true;
+    }
+    return server;
+}
--- packages/deviceconnect-manager/index.js
+++ packages/deviceconnect-manager/index.js
@@ -3,17 +3,25 @@ var https = require('https');
 var fs = require('fs');
 var manager = require('./deviceconnect-manager');
 var config = require('./config');
-var httpPort = config.httpServer.port;
-var httpsPort = config.httpsServer.port;
-var certificate = config.httpsServer.certificate;
-var privateKey = config.httpsServer.privateKey;
 
-http.createServer(manager).listen(httpPort);
-https.createServer({
-  cert: fs.readFileSync(certificate),
-  key: fs.readFileSync(privateKey)
-}, manager).listen(httpsPort);
+if (config.http.enabled === true) {
+    http.createServer(manager).listen(config.http.port);
+}
+if (config.https.enabled === true) {
+    https.createServer({
+      cert: fs.readFileSync(config.https.certificate),
+      key: fs.readFileSync(config.https.privateKey)
+    }, manager).listen(config.https.port);
+    
+}
 
-console.log('Device Connect Manager is running on:\n'
-  + '    - http://localhost:' + httpPort + '\n'
-  + '    - https://localhost:' + httpsPort);
+if (config.http.enabled === true || config.https.enabled === true) {
+    var message = 'Device Connect Manager is running on:\n';
+    if (config.http.enabled === true) {
+        message += '    - http://localhost:' + config.http.port + '\n'
+    }
+    if (config.https.enabled === true) {
+        message += '    - https://localhost:' + config.https.port + '\n'
+    }
+    console.log(message);
+}
