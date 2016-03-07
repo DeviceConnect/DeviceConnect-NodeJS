02605ea52cd9341a4ba2726da48b0453e58a77b6
Masaru Takano <mtaka@gclue.jp>
2016-03-07 14:42:26 +0900

    Add a 'query' property to Request class.

--- packages/deviceconnect-manager/request.js
+++ packages/deviceconnect-manager/request.js
@@ -4,6 +4,7 @@ var Request = function(expressReq) {
     this.profile = expressReq.params.profile;
     this.interface = expressReq.params.interface;
     this.attribute = expressReq.params.attribute;
+    this.query = expressReq.query;
 }
 
 module.exports = Request;
