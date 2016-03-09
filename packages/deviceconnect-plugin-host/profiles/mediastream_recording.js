832774b2f7037a221c8e39b862b5f1fbdf9af50d
TakayukiHoshi1984 <hoshi@gclue.jp>
2016-03-09 09:19:42 +0000

    Add onGetMediaRecorder for deviceconnect-plugin-host.

--- packages/deviceconnect-plugin-host/profiles/mediastream_recording.js
+++ packages/deviceconnect-plugin-host/profiles/mediastream_recording.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+var v4l2camera = require("v4l2camera");
+var config = JSON.parse(fs.readFileSync(__dirname + '/mediarecorder.json', 'utf8'));
+
+//for (var i = 0; i < config.recorders.length; i++) {
+//    console.log("name:" + config.recorders[i].name);
+//    console.log("module:" + config.recorders[i].module);
+//    console.log("type:" + config.recorders[i].type);
+//}
+
 module.exports = {
 
     name: 'mediastream_recording',
@@ -25,7 +35,43 @@ module.exports = {
 };
 
 function onGetMediaRecorder(request, response) {
-    response.error(1, 'To be implemented.');
+    var recorders = [];
+    for (var i = 0; i < config.recorders.length; i++) {
+        var recorder = {};
+        recorder.id = i;
+        recorder.name = config.recorders[i].name;
+	recorder.state = "inactive";
+        if (config.recorders[i].type == "camera") {
+            recorder.mimeType =  "image/jpeg";
+        } else {
+            recorder.mimeType = "audio/wav";
+        }
+        var cam;
+	try {
+	    cam = new v4l2camera.Camera(config.recorders[i].module);
+	    recorder.previewWidth = cam.configGet().width;
+ 	    recorder.previewHeight = cam.configGet().height;
+	} catch (e) {
+	    if (config.recorders[i].type == "camera") {
+		var previews = config.recorders[i].previewSizes;
+		if (previews && previews.length > 0) {
+		    recorder.previewWidth = previews[0].width;
+		    recorder.previewHeight = previews[0].height;
+		}		
+	    } else {
+		var audio = config.recorders[i].audio;
+		if (audio && audio.channel && audio.sampleRate && audio.sampleSize && audio.blockSize) {
+		    recorder.audio.channels = audio.channels;
+		    recorder.audio.sampleRate = audio.sampleRate;
+		    recorder.audio.sampleSize = audio.sampleSize;
+		    recorder.audio.blockSize = audio.blockSize;
+		}		
+	    }
+	}
+        recorders.push(recorder);
+    }
+    response.put("recorders", recorders);
+    response.ok();
 }
 
 function onPutPreview(request, response) {
